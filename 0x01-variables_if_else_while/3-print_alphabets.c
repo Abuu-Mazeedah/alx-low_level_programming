5e34ddeb1724763ed46117429ce3f39a120415f6
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-09 13:36:02 +0000

    cisfun

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	for(i = 'a'; i <= 'z'; i++){
+		putchar(i);
+	}
+	for(j = 'A'; j <= 'Z' j++)
+		putchar(j);
+	putchar("\n");
+	return (0);
+}
