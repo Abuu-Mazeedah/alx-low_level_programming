8779bc54e2007b60b58bfbe346fcc03d713462d1
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-09 15:23:14 +0000

    cisfun

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -7,6 +9,7 @@
 int main(void)
 {
 	int j;
+
 	for (j = 0; j < 10; j++)
 	{
 		printf("%d", j);
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** 
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -20,4 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
