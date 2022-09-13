2c1ac830723322319d593a435ff05365a480c676
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-13 23:08:19 +0000

    mm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	int alpha;
 	int alpha2;
-	
+
 	for (alpha = 0; alpha <= 9; alpha++)
 	{
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
@@ -17,5 +17,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-	return 0;
 }
