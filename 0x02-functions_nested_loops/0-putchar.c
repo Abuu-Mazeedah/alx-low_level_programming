c3f8930d44adab9a9637bff1bd2834bcdf7c2936
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-13 20:51:24 +0000

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char container [10] = "_putchar";
+	for(int i = 0; i < 8; i++)
+	{
+		putchar(container[i]);
+	}
+	putchar('\n');
+	return (0);
+}
