8199be971c7065abfd73e89ab5d44f8e53b6dbf3
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-13 22:42:37 +0000

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int alpha;
+	int alpha2;
+	
+	for (alpha = 0; alpha <= 9; alpha++)
+	{
+		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
+		{
+			_putchar(alpha2);
+		}
+		_putchar('\n');
+	}
+}
