77bb64adeb387c5313e7c807083b64b6dc35f9b4
Adeleke Anuoluwa <hernuincredible@gmail.com>
2022-09-14 00:08:31 +0000

    mmm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 			}
 			if (y == 0)
 				_putchar('0');
-			else if (y >= 10)
+			else if (product >= 10)
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
